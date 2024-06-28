8317022381a58815a5dd6bac783c354885b449cb
isaias-trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2024-06-28 09:48:48 -0500

    tooltip added to option in web version

--- src/components/option/Option.tsx
+++ src/components/option/Option.tsx
@@ -1,20 +1,36 @@
 import {FC} from "react";
-import {cn, Radio} from "@nextui-org/react";
+import {cn, Radio, Tooltip} from "@nextui-org/react";
 import OptionType from "../../types/OptionType";
 
-const Option: FC<OptionType> = ({uuid, weight}) => {
-    return <Radio
-        value={uuid}
-        color="default"
-        className="flex flex-col-reverse place-content-center rounded-xl max-sm:p-2.5" classNames={{
-        base: cn(
-            "data-[selected=true]:bg-container max-sm:shadow-lg max-sm:data-[selected=true]:shadow-none max-sm:data-[selected=true]:border-1 max-sm:data-[selected=true]:border-black",
-        ),
-        wrapper: cn("max-sm:hidden")
-    }}
-    >
-        <span className="max-sm:text-medium text-center self-center justify-self-center w-min h-min ">{weight}</span>
-    </Radio>
+const Option: FC<OptionType> = ({uuid, weight, name}) => {
+    return <Tooltip
+        content={name}
+        placement='top'
+        showArrow={true}
+        offset={-5}
+        color="foreground">
+
+        <Radio
+            value={uuid}
+            color="default"
+            classNames={{
+                base: cn(
+                    "hover:bg-on-container",
+                    "max-sm:max-w-9 max-sm:max-h-9",
+                    "flex flex-row flex-col-reverse rounded-xl",
+                    'max-sm:p-2.5 gap-1.5',
+                    "data-[selected=true]:bg-container",
+                    'max-sm:shadow-lg max-sm:data-[selected=true]:shadow-none',
+                    'max-sm:data-[selected=true]:border-1 max-sm:data-[selected=true]:border-black'
+                ),
+                wrapper: cn("max-sm:hidden"),
+                label: cn("flex mr-1.5 max-sm:-mb-1.5")
+            }}
+        >
+            {weight}
+        </Radio>
+    </Tooltip>
 };
+
 
 export default Option
