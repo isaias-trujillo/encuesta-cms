3d76ffd4141fcf50e5e6cac1f2ec0f72d9fd7963
isaias-trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2024-07-03 12:10:36 -0500

    minor style change

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import styles from './styles.module.css'
+import {Image} from "@nextui-org/react";
 
 type Props = {
     status: 'error' | 'not found';
@@ -13,8 +14,14 @@ const ErrorPage: FC<Props> = ({status, message}) => {
             {status === "not found" ? "404" : "Algo fue mal..."}
         </span>
         <div className="flex justify-center items-center">
-            <span className={styles.message + " text-background"}>{message}</span>
+            <span className={`text-violet-200 ${styles.message}`}>{message}</span>
             <span className={styles.spacer}></span>
+        </div>
+        <div
+            className="text-white absolute bottom-0 flex flex-col w-full lg:px-20 md:px-10 px-5 lg:py-5  py-2.5 md:text-xl max-md:text-medium text-xs font-normal font-['Roboto']">
+            <Image src="/pics/logo_cms_aniversario.png" className="max-h-20" alt="Logo de la Clínica"/>
+            <p className="text-xs">Copyright © 2024 – Clinica Maison de Santé.</p>
+            <p className="text-xs">Todos los derechos reservados.</p>
         </div>
     </div>
 }
