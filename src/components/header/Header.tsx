99fcef289d6b5aeeb009724729a86a7191b4bec0
isaias-trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2024-07-24 15:56:22 -0500

    service and date cared added after the main content

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,43 @@
 import {FC} from "react";
+import useForm from "../../stores/useForm.ts";
 
 const Header: FC = () => {
-    return <main
-        className="flex flex-col lg:p-10 p-4 justify-center place-items-start bg-dark-blue rounded-medium text-white">
+    const service = useForm(state => state.data()?.service);
+
+    return <div className='flex flex-col gap-2.5'>
+        <main
+            className="flex flex-col lg:p-8 p-4 lg:gap-2.5 gap-1.5 justify-center place-items-start bg-dark-blue rounded-medium text-white">
             <span
                 className="flex flex-row lg:gap-1 gap-0.5 flex-wrap font-medium lg:text-4xl text-2xl font-['Roboto Condensed']">
                 Encuesta de Satisfacción <h1 className="text-bright-orange"> Clínica Maison de Santé</h1></span>
-        <p className="text-base max-md:text-sm font-['Roboto']">
-            Estimado usuario (a), estamos interesados en conocer su opinión sobre la calidad de atención que recibió
-            en la Clínica Maison de Santé. Sus respuestas son totalmente confidenciales. Agradeceremos su
-            participación.
-        </p>
-    </main>
+            <p className="text-md max-md:text-sm font-['Roboto']">
+                Estimado usuario (a), estamos interesados en conocer su opinión sobre la calidad de atención que recibió
+                en la Clínica Maison de Santé. Sus
+                respuestas son totalmente confidenciales.
+                Agradeceremos su
+                participación.
+            </p>
+        </main>
+        {
+            service && <article
+                className="flex flex-row  lg:gap-2 gap-1.5 flex-wrap text-md max-md:text-sm font-['Roboto'] bg-white rounded-medium text-dark-blue lg:p-5 p-4 justify-between">
+                <div className='inline-flex gap-2.5 items-center'>
+                    <img
+                        className='w-5 h-5'
+                        src="https://cdn-icons-png.flaticon.com/512/1040/1040238.png"
+                        alt={service.name}
+                    /> <b>Servicio:</b>{service?.['name']}
+                </div>
+                <div className='inline-flex gap-2.5 items-center'>
+                    <img
+                        className='w-5 h-5'
+                        src="https://cdn-icons-png.flaticon.com/512/10473/10473587.png"
+                        alt={service['care date']}
+                    /> <b>Fecha de atención:</b>{service['care date']}
+                </div>
+            </article>
+        }
+    </div>
 }
 
 export default Header;
